42faba7d4cb50ee40709a23b41027e05822180b3
Lê Đình Thắng <ledinhthang102@gmail.com>
2018-12-06 09:12:28 +0700

    Change update script and add some extension

--- add-site-linux.sh
+++ add-site-linux.sh
@@ -2,36 +2,36 @@
 
 # This shell to quick command create a site in nginx and apache
 echo 'Enter your site name (name of project root)? : '; 
-read site_name;
+read -r site_name;
 
-parent_dir=$(dirname $PWD);
+parent_dir=$(dirname "$PWD");
 # Check exist file in project root
 if [ ! -d "$parent_dir/$site_name" ]; then
-    mkdir $parent_dir/$site_name;
+    mkdir "$parent_dir/$site_name"
     # Add to hosts
-    sudo cp /etc/hosts /etc/hosts-bak
-    sudo echo 127.0.0.1 $site_name.local>>/etc/hosts
+    cp "/etc/hosts" "/etc/hosts-bak"
+    echo "127.0.0.1 $site_name.local" | sudo tee -a /etc/hosts
 else
-    echo 'Folder Exist';
+    echo 'Folder Exist'
 fi
 # Create site in apache
-cp apache2/sites/sample.conf.example apache2/sites/$site_name.conf;
+cp "apache2/sites/sample.conf.example" "apache2/sites/$site_name.conf"
 
-sed -i "s/sample.test/$site_name.local/g" apache2/sites/$site_name.conf;
-sed -i "s/\/var\/www\/sample\/public\//\/var\/www\/$site_name\//g" apache2/sites/$site_name.conf;
+sed -i "s/sample.test/$site_name.local/g" "apache2/sites/$site_name.conf"
+sed -i "s/\/var\/www\/sample\/public\//\/var\/www\/$site_name\//g" "apache2/sites/$site_name.conf"
 
 # Create site in nginx
 
-cp nginx/sites/app.conf.example nginx/sites/$site_name.conf;
+cp "nginx/sites/app.conf.example" "nginx/sites/$site_name.conf"
 
-sed -i "s/server_name app.test/server_name $site_name.local/g" nginx/sites/$site_name.conf;
-sed -i "s/root \/var\/www\/app/root \/var\/www\/$site_name/g" nginx/sites/$site_name.conf;
-sed -i "s/app_error.log/$site_name\_error.log/g" nginx/sites/$site_name.conf;
-sed -i "s/app_access.log/$site_name\_access.log/g" nginx/sites/$site_name.conf;
+sed -i "s/server_name app.test/server_name $site_name.local/g" "nginx/sites/$site_name.conf"
+sed -i "s/root \/var\/www\/app/root \/var\/www\/$site_name/g" "nginx/sites/$site_name.conf"
+sed -i "s/app_error.log/$site_name\_error.log/g" "nginx/sites/$site_name.conf"
+sed -i "s/app_access.log/$site_name\_access.log/g" "nginx/sites/$site_name.conf"
 
-echo "Site Name: $site_name.local";
-echo "Dir Path: $parent_dir/$site_name";
-echo "Press any key to continues ... !";
-read anykey;
-code $parent_dir/$site_name
-exit 1;
+echo "Site Name: $site_name.local"
+echo "Dir Path: $parent_dir/$site_name"
+echo "Press any key to continues ... !"
+read -r
+code "$parent_dir/$site_name"
+exit 1
--- start-lemp.sh
+++ start-lemp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Start lamp for linux enviroment
-docker-compose start nginx php-fpm mysql phpmyadmin workspace
+docker-compose up -d nginx php-fpm mysql phpmyadmin workspace
 
 php-fpm/xdebug status
 php-fpm/xdebug stop
 echo 'Xdebug is stop'
-echo 'press any key to continues .... !';
-read k;
+echo 'press any key to continues .... !'
+read -r
